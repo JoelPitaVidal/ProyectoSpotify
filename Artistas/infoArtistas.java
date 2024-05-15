13130388ecaa0d6446a22d1be25534dac7a6e27b
JoelPitaVidal <jpitavidal@daielcastelao.org>
2024-05-15 18:49:40 +0200

    inicio de formateo de respuestas

--- Artistas/infoArtistas.java
+++ Artistas/infoArtistas.java
@@ -18,76 +18,76 @@ public class infoArtistas extends JFrame {
     final String ARTISTA9 = "Eminem";
     final String ARTISTA10 = "Rob Zombie";
 
-    final String INFOARTISTA1 = "\"La información de Ghostmane es : \\n\" +\n" +
-            "                        \"Nombre real: Eric Ghoste Whitney \\n\" +\n" +
-            "                        \"Fecha de nacimiento: 15 de abril de 1991 \\n \" +\n" +
-            "                        \"Origen: Lake Worth, Florida, Estados Unidos|\\n\" +\n" +
-            "                        \"Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal\" +\n" +
-            "                        \"Sitio Web: ghostemanemerch.online\"";
+    final String INFOARTISTA1 = "La información de Rob Zombie es : \n" +
+            "\nNombre real: Eric Ghoste Whitney\n" +
+            "\nFecha de nacimiento: 15 de abril de 1991\n" +
+            "\nOrigen: Lake Worth, Florida, Estados Unidos\n" +
+            "\nGéneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal+\n" +
+            "\nSitio Web: ghostemanemerch.online";
 
 
-    final String INFOARTISTA2 = "\"La información de Nigth Lovel es : \\n\" +\n" +
-            "                        \"Nombre real: Eric Ghoste Whitney \\n\" +\n" +
-            "                        \"Fecha de nacimiento: 15 de abril de 1991 \\n \" +\n" +
-            "                        \"Origen: Lake Worth, Florida, Estados Unidos|\\n\" +\n" +
-            "                        \"Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal\" +\n" +
-            "                        \"Sitio Web: ghostemanemerch.online";
+    final String INFOARTISTA2 = "La información de Rob Zombie es : \n" +
+            "Nombre real: Eric Ghoste Whitney\n" +
+            "Fecha de nacimiento: 15 de abril de 1991\n" +
+            "Origen: Lake Worth, Florida, Estados Unidos\n" +
+            "Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal+\n" +
+            "Sitio Web: ghostemanemerch.online";
 
-    final String INFOARTISTA3 = "\"La información de Snoop Dog es : \\n\" +\n" +
-            "                        \"Nombre real: Eric Ghoste Whitney \\n\" +\n" +
-            "                        \"Fecha de nacimiento: 15 de abril de 1991 \\n \" +\n" +
-            "                        \"Origen: Lake Worth, Florida, Estados Unidos|\\n\" +\n" +
-            "                        \"Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal\" +\n" +
-            "                        \"Sitio Web: ghostemanemerch.online\"";
+    final String INFOARTISTA3 = "La información de Rob Zombie es : \n" +
+            "Nombre real: Eric Ghoste Whitney\n" +
+            "Fecha de nacimiento: 15 de abril de 1991\n" +
+            "Origen: Lake Worth, Florida, Estados Unidos\n" +
+            "Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal+\n" +
+            "Sitio Web: ghostemanemerch.online";
 
-    final String INFOARTISTA4 = "\"La información de Blackpink es : \\n\" +\n" +
-            "                        \"Nombre real: Eric Ghoste Whitney \\n\" +\n" +
-            "                        \"Fecha de nacimiento: 15 de abril de 1991 \\n \" +\n" +
-            "                        \"Origen: Lake Worth, Florida, Estados Unidos|\\n\" +\n" +
-            "                        \"Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal\" +\n" +
-            "                        \"Sitio Web: ghostemanemerch.online\"";
+    final String INFOARTISTA4 = "La información de Rob Zombie es : \n" +
+            "Nombre real: Eric Ghoste Whitney\n" +
+            "Fecha de nacimiento: 15 de abril de 1991\n" +
+            "Origen: Lake Worth, Florida, Estados Unidos\n" +
+            "Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal+\n" +
+            "Sitio Web: ghostemanemerch.online";
 
-    final String INFOARTISTA5 = "\"La información de Cartel de Santa es : \\n\" +\n" +
-            "                        \"Nombre real: Eric Ghoste Whitney \\n\" +\n" +
-            "                        \"Fecha de nacimiento: 15 de abril de 1991 \\n \" +\n" +
-            "                        \"Origen: Lake Worth, Florida, Estados Unidos|\\n\" +\n" +
-            "                        \"Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal\" +\n" +
-            "                        \"Sitio Web: ghostemanemerch.online\"";
+    final String INFOARTISTA5 ="La información de Rob Zombie es : \n" +
+            "Nombre real: Eric Ghoste Whitney\n" +
+            "Fecha de nacimiento: 15 de abril de 1991\n" +
+            "Origen: Lake Worth, Florida, Estados Unidos\n" +
+            "Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal+\n" +
+            "Sitio Web: ghostemanemerch.online";
 
-    final String INFOARTISTA6 = "\"La información de Extremo duro es : \\n\" +\n" +
-            "                        \"Nombre real: Eric Ghoste Whitney \\n\" +\n" +
-            "                        \"Fecha de nacimiento: 15 de abril de 1991 \\n \" +\n" +
-            "                        \"Origen: Lake Worth, Florida, Estados Unidos|\\n\" +\n" +
-            "                        \"Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal\" +\n" +
-            "                        \"Sitio Web: ghostemanemerch.online\"";
+    final String INFOARTISTA6 = "La información de Rob Zombie es : \n" +
+            "Nombre real: Eric Ghoste Whitney\n" +
+            "Fecha de nacimiento: 15 de abril de 1991\n" +
+            "Origen: Lake Worth, Florida, Estados Unidos\n" +
+            "Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal+\n" +
+            "Sitio Web: ghostemanemerch.online";
 
-    final String INFOARTISTA7 = "\"La información de CORPSE es : \\n\" +\n" +
-            "                        \"Nombre real: Eric Ghoste Whitney \\n\" +\n" +
-            "                        \"Fecha de nacimiento: 15 de abril de 1991 \\n \" +\n" +
-            "                        \"Origen: Lake Worth, Florida, Estados Unidos|\\n\" +\n" +
-            "                        \"Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal\" +\n" +
-            "                        \"Sitio Web: ghostemanemerch.online\"";
+    final String INFOARTISTA7 = "La información de Rob Zombie es : \n" +
+            "Nombre real: Eric Ghoste Whitney\n" +
+            "Fecha de nacimiento: 15 de abril de 1991\n" +
+            "Origen: Lake Worth, Florida, Estados Unidos\n" +
+            "Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal+\n" +
+            "Sitio Web: ghostemanemerch.online";
 
-    final String INFOARTISTA8 = "\"La información de Disturbed es : \\n\" +\n" +
-            "                        \"Nombre real: Eric Ghoste Whitney \\n\" +\n" +
-            "                        \"Fecha de nacimiento: 15 de abril de 1991 \\n \" +\n" +
-            "                        \"Origen: Lake Worth, Florida, Estados Unidos|\\n\" +\n" +
-            "                        \"Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal\" +\n" +
-            "                        \"Sitio Web: ghostemanemerch.online\"";
+    final String INFOARTISTA8 = "La información de Rob Zombie es : \n" +
+            "Nombre real: Eric Ghoste Whitney\n" +
+            "Fecha de nacimiento: 15 de abril de 1991\n" +
+            "Origen: Lake Worth, Florida, Estados Unidos\n" +
+            "Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal+\n" +
+            "Sitio Web: ghostemanemerch.online";
 
-    final String INFOARTISTA9 = "\"La información de Eminem es : \\n\" +\n" +
-            "                        \"Nombre real: Eric Ghoste Whitney \\n\" +\n" +
-            "                        \"Fecha de nacimiento: 15 de abril de 1991 \\n \" +\n" +
-            "                        \"Origen: Lake Worth, Florida, Estados Unidos|\\n\" +\n" +
-            "                        \"Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal\" +\n" +
-            "                        \"Sitio Web: ghostemanemerch.online\"";
+    final String INFOARTISTA9 ="La información de Rob Zombie es : \n" +
+            "Nombre real: Eric Ghoste Whitney\n" +
+            "Fecha de nacimiento: 15 de abril de 1991\n" +
+            "Origen: Lake Worth, Florida, Estados Unidos\n" +
+            "Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal+\n" +
+            "Sitio Web: ghostemanemerch.online";
 
-    final String INFOARTISTA10 = "\"La información de Rob Zombie es : \\n\" +\n" +
-            "                        \"Nombre real: Eric Ghoste Whitney \\n\" +\n" +
-            "                        \"Fecha de nacimiento: 15 de abril de 1991 \\n \" +\n" +
-            "                        \"Origen: Lake Worth, Florida, Estados Unidos|\\n\" +\n" +
-            "                        \"Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal\" +\n" +
-            "                        \"Sitio Web: ghostemanemerch.online\"";
+    final String INFOARTISTA10 = "La información de Rob Zombie es : \n" +
+            "Nombre real: Eric Ghoste Whitney\n" +
+            "Fecha de nacimiento: 15 de abril de 1991\n" +
+            "Origen: Lake Worth, Florida, Estados Unidos\n" +
+            "Géneros musicales: Dark trap, noise, hip hop, trap, hip hop, cloud rap, rap Metal+\n" +
+            "Sitio Web: ghostemanemerch.online";
 
 
 
@@ -164,7 +164,7 @@ public class infoArtistas extends JFrame {
                         mostrarInfo.setText(INFOARTISTA9);
                     } else if (textoBuscado.equals(ARTISTA10)) {
                         mostrarInfo.setText(INFOARTISTA10);
-                    }
+                    }else mostrarInfo.setText("Artista desconocido");
                 }
             }
         });
